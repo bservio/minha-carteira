d2bbe67d76194a99e82ca21d267627f443912574
Bruno Sérvio Barbosa <brunoservio21@gmail.com>
2022-08-07 19:17:55 -0300

    Criação e estilização do componente FinanceCard. Adequação de List e Content

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,28 +1,53 @@
 import React from 'react';
-import { Container } from './styles';
+import { Container, Content, Filters } from './styles';
 
 import SelectInput from '../../components/SelectInput';
+import FinanceCard from '../../components/FinanceCard';
 import ContentHeader from '../../components/ContentHeader';
 
 const List: React.FC = () => {
-	const names = [
-		{ value: "Bruno", label: "Bruno" },
-		{ value: "Ana", label: "Ana" },
-		{ value: "José", label: "José" }
+	const months = [
+		{ value: 8, label: "Agosto" },
+		{ value: 9, label: "Setembro" },
+		{ value: 10, label: "Outubro" }
 	]
 
-	const places = [
-		{ value: "Batalha", label: "Batalha" },
-		{ value: "Teresina", label: "Teresina" },
-		{ value: "Parnaíba", label: "Parnaíba" },
+	const years = [
+		{ value: 2023, label: "2023" },
+		{ value: 2024, label: "2024" },
+		{ value: 2025, label: "2025" },
 	]
 
 	return (
 		<Container>
-			<ContentHeader title="List" lineColor="#F7931B">
-				<SelectInput options={names} />
-				<SelectInput options={places} />
+			<ContentHeader title="Saídas" lineColor="#E44C4E">
+				<SelectInput options={months} />
+				<SelectInput options={years} />
 			</ContentHeader>
+
+			<Filters>
+				<button
+					type="button"
+					className='tag-filter tag-filter-recurrent'
+				>
+					Recorrentes
+				</button>
+				<button
+					type="button"
+					className='tag-filter tag-filter-eventual'
+				>
+					Eventuais
+				</button>
+			</Filters>
+
+			<Content>
+				<FinanceCard
+					tagColor='#E44C4E'
+					title='Conta de Luz'
+					subtitle='27/08/2022'
+					amount='R$ 320,00'
+				/>
+			</Content>
 		</Container>
 	)
 }
